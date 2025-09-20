dadf84c8feb7f06bb3392bd0ebc1b122ac343a14
abdulvahabaa <abdulvahabaa916@gmail.com>
2025-09-20 23:36:46 +0530

    feat:inital local setup with view engine

---
+++ server.js
@@ -0,0 +1,75 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import multer from "multer";
+import { engine } from "express-handlebars";
+import authRoutes from "./routes/users/authRoutes.js";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+app.use(cors());
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILE STORAGE */
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+/* ROUTES WITH FILES */
+// app.post("/auth/register",upload.single("picture"),register);
+// app.post("/posts",upload.single("picture"),createPost);
+
+/* ROUTES */
+app.use("/api/auth", authRoutes);
+// app.use("/users",userRoutes);
+
+// app.get("/", (req, res) => {
+//     res.send("Hello from Homepage");
+// })
+
+
+app.listen(PORT, () => {
+  console.log(
+    `process ID ${process.pid}:server running on PORT ${PORT} in dev mode`
+  );
+});
+
+/* VIEW ENGINE SETUP */
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs", // use .hbs extension
+    defaultLayout: "main", // default layout file (main.hbs)
+    layoutsDir: path.join(__dirname, "views/layouts"), // layouts folder
+    partialsDir: path.join(__dirname, "views/partials"), // partials folder
+    helpers: {
+      // custom helpers for production scaling
+      upper: (str) => str.toUpperCase(),
+      json: (context) => JSON.stringify(context),
+    },
+  })
+);
+
+// set view engine
+app.set("view engine", "hbs");
+// set views folder
+app.set("views", path.join(__dirname, "views"));
