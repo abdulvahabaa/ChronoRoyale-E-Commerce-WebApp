2602c605b0c8fabe0ad27f8678fd440bedcf9c6e
abdulvahabaa <abdulvahabaa916@gmail.com>
2025-09-28 13:25:22 +0530

    feat:add product form corrected

--- controllers/productsControllers/productController.js
+++ controllers/productsControllers/productController.js
@@ -1,16 +1,37 @@
 import connectToDatabase from "../../config/db.js";
 import collection from "../../config/collection";
 
-export const addProduct  = async (req,res)=>{
-    try {
-        const data = req.body
-        console.log(data);
+export const addProduct = async (req, res) => {
+    console.log("Add Product route working 🚀");
+  try {
+    const data = req.body;
 
-        const db = await connectToDatabase(process.env.DATABASE);
-        const result = await db.collection(collection.PRODUCTS_COLLECTION).insertOne(data);
-        console.log(result);
-        
-    } catch (error) {
-        
-    }
-}
+    const productData = {
+      name: data.name,
+      shortDesc: data.shortDesc,
+      description: data.description,
+      category: data.category,
+      brand: data.brand,
+      price: data.price,
+      discountPrice: data.discountPrice,
+      stock: data.stock,
+      rating: "",
+      picturePath: data.images,
+      thumbnail: data.thumbnail,
+      status: data.status,
+      createdAt: Date().now(),
+      updatedAt: Date().now(),
+      isDelete: false,
+    };
+
+    console.log(productData);
+
+    // const db = await connectToDatabase(process.env.DATABASE);
+    // const result = await db
+    //   .collection(collection.PRODUCTS_COLLECTION)
+    //   .insertOne();
+    // console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
